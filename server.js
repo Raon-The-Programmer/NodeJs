2b2d0a8ab350bcea3ccce1835364175631973e27
Rahul John <msdrahuljohn@gmail.com>
2023-12-02 21:51:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+const folderPath = path.join(__dirname, 'new'); 
+
+// Create folder if not exists
+if (!fs.existsSync(folderPath)) {
+  fs.mkdirSync(folderPath);
+}
+
+// API endpoint to create a text file with the current timestamp
+app.post('/createFile', (req, res) => {
+  const currentTimestamp = new Date().toISOString().replace(/:/g, '-'); // Remove ':' from timestamp for Windows compatibility
+  const fileName = `${currentTimestamp}.txt`;
+  const filePath = path.join(folderPath, fileName);
+
+  fs.writeFile(filePath, currentTimestamp, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.status(201).json({ message: 'File created successfully', fileName });
+    }
+  });
+});
+
+// API endpoint to retrieve all text files in the folder
+app.get('/getAllFiles', (req, res) => {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json({ files });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
